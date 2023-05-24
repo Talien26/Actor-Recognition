600a609592fd19b20e3d49063a9136ff9eec98eb
Talien26 <101036443+Talien26@users.noreply.github.com>
2023-05-24 15:28:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+camera = document.getElementById("camera");
+Webcam.attach ('#camera');
+
+Webcam.set({
+    width:450,
+    height:320,
+    image_format : 'png',
+    png_quality : 90
+});
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+console.log('ML5 Version : ', ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/0i3S_G0fW/',modelLoaded);
+
+function modelLoaded(){
+    console.log('The Model has been loaded!');
+};
+
+function check(){
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+function gotResult(error, results) {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log(results);
+      document.getElementById("result_object_name").innerHTML = results[0].label;
+      document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+  }
